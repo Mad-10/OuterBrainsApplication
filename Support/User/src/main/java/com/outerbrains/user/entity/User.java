965d7f6a46c620dd63dccc0fbe0c9788818861d4
Alan Ivan <2728946156@qq.com>
2023-05-01 23:58:07 +0800

    为User添加头像属性；并删除不必要的构造函数。

--- Support/User/src/main/java/com/outerbrains/user/entity/User.java
+++ Support/User/src/main/java/com/outerbrains/user/entity/User.java
@@ -4,7 +4,7 @@ import com.outerbrains.base.entity.OuterBrainsObject;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.util.ArrayList;
+import java.io.File;
 import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
@@ -14,23 +14,6 @@ public class User extends OuterBrainsObject {
     private List<Contact> contacts;
     private String email;
     private Role role;
+    private File avatar;//用户头像
 
-    public User() {
-        super();
-        contacts = new ArrayList<>();
-    }
-
-    public User(String name, String description, String password, List<Contact> contacts, String email) {
-        super(name, description);
-        this.password = password;
-        this.contacts = contacts;
-        this.email = email;
-    }
-    public User(String name, String password, List<Contact> contacts, String email, Role role) {
-        super(name);
-        this.password = password;
-        this.contacts = contacts;
-        this.email = email;
-        this.role = role;
-    }
 }
